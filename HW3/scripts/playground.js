e6d235ee8cd1c5ade12ac991154e88602098ceed
Brandon Su <brandonsu@berkeley.edu>
2023-10-06 17:29:57 -0700

    finished hw3

--- HW3/scripts/playground.js
+++ HW3/scripts/playground.js
@@ -1,7 +1,7 @@
 // Part 2
 const countDisplay = document.getElementById("count-display");
-const subtractButton = document.getElementById("subtract")
-const addButton = /* YOUR CODE HERE */
+const subtractButton = document.getElementById("subtract");
+const addButton = document.getElementById("add");
 
 
 
@@ -11,7 +11,7 @@ let count = 0;
 
 function increaseCount() {
   /*Increment the count variable by 1.*/
-  /* YOUR CODE HERE */
+  count = count + 1;
   countDisplay.textContent = count;
 }
 
@@ -19,15 +19,19 @@ function increaseCount() {
 // Part 3b:
 
 /* Fill in the appropriate arguments for the addEventListener function. Reference the link in the spec for help! */
-addButton.addEventListener(/* YOUR CODE HERE */, /* YOUR CODE HERE */);
+addButton.addEventListener("click", increaseCount);
 
 
 
 // Part 3c:
 
 // Write subtractButton's event listener
-/* YOUR CODE HERE */
+function decreaseCount() {
+  count = count - 1;
+  countDisplay.textContent = count;
+}
 
+subtractButton.addEventListener("click", decreaseCount);
 
 
 
@@ -36,9 +40,9 @@ addButton.addEventListener(/* YOUR CODE HERE */, /* YOUR CODE HERE */);
 const inputTask = document.getElementById("input-task");
 const taskList = document.getElementById("tasks");
 // Initialize the const submitButton:
-/* YOUR CODE HERE */
+const submitButton = document.getElementById("submit");
 // Initialize the const clearButton:
-/* YOUR CODE HERE */
+const clearButton = document.getElementById("clear");
 
 
 
@@ -47,10 +51,10 @@ const taskList = document.getElementById("tasks");
 submitButton.addEventListener("click", () => {
   if (inputTask.value != "") {
       // Initialize a const variable named "task", and set it equal to a new li element. 
-      /* YOUR CODE HERE */
+      const task = document.createElement('li');
       task.textContent = inputTask.value; // Here you set the li element you just created to have the text value in the input field!
       // Add this task to the end of taskList (this is the list you initialized earlier!).
-      taskList./* YOUR CODE HERE*/
+      taskList.appendChild(task);
       inputTask.value = "";
   }
 });
@@ -60,7 +64,7 @@ clearButton.addEventListener("click", () => {
   inputTask.value = "";
 });
 
-taskName.addEventListener("input", () => {
+inputTask.addEventListener("input", () => {
   if (inputTask.value == "Cal Hacks") {
     countDisplay.style.color = "steelblue";
   }
