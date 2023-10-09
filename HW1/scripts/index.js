e756cd047fd89779e3481c22c160e7d65b6cabf8
Brandon Su <brandonsu@berkeley.edu>
2023-10-09 16:49:02 -0700

    created directory page

---
+++ HW1/scripts/index.js
@@ -0,0 +1,7 @@
+const video = document.getElementById("video");
+const body = document.getElementById("body");
+
+video.addEventListener("click", () => {
+    body.style.backgroundImage = "url('assets/twice.jpg')";
+
+});
